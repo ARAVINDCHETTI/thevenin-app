753399012779749867d9bbbd6f1657c444d44491
ARAVINDCHETTI <156351992+ARAVINDCHETTI@users.noreply.github.com>
2024-11-08 18:14:06 +0530

    Update theromMain.py

---
+++ theromMain.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("thevenin therom done in section")
+vth=st.number_input("Th volatge")
+Rth=st.number_input("Rth")
+Rl=st.number_input("R load")
+
+
+st.button("run")
